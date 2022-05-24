873da7feecd2f2339f6f3dd9f8adb245875d2ef1
5656baba <95210206+5656baba@users.noreply.github.com>
2022-05-25 02:01:03 +0900

    studied way to java compile

---
+++ sample.java
@@ -0,0 +1,7 @@
+public class sample {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+    System.out.println("Hello WEBCAMP!");
+    System.out.println("Hello Java!");
+  }
+}
