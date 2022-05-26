ca5535c5e2fe80e9301a38cd91d90eb1ee11773e
5656baba <95210206+5656baba@users.noreply.github.com>
2022-05-27 06:39:14 +0900

    i studied about variable

---
+++ Variable.java
@@ -0,0 +1,18 @@
+public class Variable {
+  public static void main(String[] args) {
+
+    //型 変数名;  変数の宣言
+    int num1;
+    String str1;
+    num1 = 10;
+    str1 = "Hello World!";
+    System.out.println(num1);
+    System.out.println(str1);
+
+    num1 = 20;
+    System.out.println(num1);
+
+    int num2 = 30;
+    System.out.println(num2);
+  }
+}
--- sample.java
+++ sample.java
@@ -5,3 +5,4 @@ public class sample {
     System.out.println("Hello Java!");
   }
 }
+
