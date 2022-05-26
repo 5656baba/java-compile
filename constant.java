ecef5075cc2678c2f60aff6930cefc86d2ea4111
5656baba <95210206+5656baba@users.noreply.github.com>
2022-05-27 06:43:23 +0900

    i studied about constant

---
+++ constant.java
@@ -0,0 +1,10 @@
+public class constant {
+  public static void main(String[] args) {
+    // 定数の宣言  final 定数の型 定数名 = 初期値;
+    final int MIN_NUMBER = 10;
+    System.out.println(MIN_NUMBER);
+
+    // 再代入しようとするとエラー
+    //MIN_NUMBER = 20;
+ }
+}
